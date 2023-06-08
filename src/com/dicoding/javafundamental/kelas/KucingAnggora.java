dcc1caa301b6feb2baff0157588bab97f5116eba
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-06-08 08:49:07 +0700

    Class

---
+++ src/com/dicoding/javafundamental/kelas/KucingAnggora.java
@@ -0,0 +1,19 @@
+package com.dicoding.javafundamental.kelas;
+
+public class KucingAnggora {
+
+    // ini adalah contoh no-argumen Constructor
+    public KucingAnggora() {
+    }
+        String namaKucing;
+        int beratBadan;
+
+        // ini adalah contoh parameterized constructor
+        // dengan memberikan nilai awak data member nama_kucing dan berat_badan
+        // dengan parameter ysng kita tentukan
+        public KucingAnggora(String nama, int berat){
+            namaKucing = nama;
+            beratBadan = berat;
+        }
+    }
+
